fc04cc301036f299e3389bf0d9822d5ed0672432
WildanKurniadi <danprogramming10@gmail.com>
2019-07-15 00:53:54 +0700

    #06 get All Mahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -2,16 +2,13 @@
 
 class Mahasiswa extends CI_Controller
 {
-   public function __construct()
-   {
-      parent::__construct();
-      $this->load->database(); //jika semua method di dalam class Mahasiswa membutuhkan koneksi ke database, maka taruh di konstruktor, jika tidak letakan saja di method yang membutuhkannya.
-   }
 
    public function index()
    {
+      //di dalam CI jangan lupa untuk load model sebelum digunakan!
+      $this->load->model('Mahasiswa_model');
       $data['judul'] = 'Data Mahasiswa';
-
+      $data['mahasiswa'] =  $this->Mahasiswa_model->getAllMahasiswa();
       $this->load->view('templates/header', $data);
       $this->load->view('mahasiswa/index');
       $this->load->view('templates/footer');
---
+++ application/models/Mahasiswa_model.php
@@ -0,0 +1,10 @@
+<?php
+
+//dengan CI, semua model harus meng-extend class CI_Model
+class Mahasiswa_model extends CI_Model
+{
+   public function getAllMahasiswa()
+   {
+      echo 'ok';
+   }
+}
