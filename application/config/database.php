82bd48a759bfb0fe295ec16c169e6885dc9018dd
WildanKurniadi <danprogramming10@gmail.com>
2019-07-15 00:55:47 +0700

    #06 Setting autoload pada database, penggunaan Models didalam CI (harus di load oleh Controller sebelum digunakan)

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+ 
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -1,6 +1,6 @@
 <?php
 
-//dengan CI, semua model harus meng-extend class CI_Model
+//dengan CI, semua model harus meng-extend class CI_Model 
 class Mahasiswa_model extends CI_Model
 {
    public function getAllMahasiswa()
