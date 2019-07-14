764df8a4a6da9dc6948ac2f5d1b64715fa0282f1
WildanKurniadi <danprogramming10@gmail.com>
2019-07-15 01:05:14 +0700

    #09 Berhasil Melakukan Get All Mahasiswa, dan tampilkan nama semua mahasiswa di dalam list

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,11 +1,11 @@
-<?php var_dump($mahasiswa) ?>
-
 <div class="container">
    <div class="row mt-3">
       <div class="col-md-6">
          <h3>Daftar Mahasiswa</h3>
          <ul class="list-group">
-            <li class="list-group-item">Item 1</li>
+            <?php foreach ($mahasiswa as $mhs) : ?>
+               <li class="list-group-item"><?= $mhs['nama'] ?></li>
+            <?php endforeach ?>
          </ul>
       </div>
    </div>
